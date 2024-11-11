aa765f286a8e91b0c362c64f0d8a5d5acf726004
PauloAhoaugi <pauloahouagi@gmail.com>
2024-11-11 16:48:06 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from './turboLogger.js';
 
 console.log('usando o github');
 turboLogger('Estou aprendendo o fluxo do git', 'red');
+turboLogger(
+  'Consegui criar uma nova versão sem afetar a principal',
+  'darkgreen'
+);
